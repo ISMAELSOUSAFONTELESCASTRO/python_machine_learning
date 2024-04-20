840030c48f6635e34eb2d5efbc274aee7d0e85ec
Ismael Sousa <ismaelsousafcastro@gmail.com>
2024-04-20 18:18:25 -0300

    Update ACO.py

---
+++ ACO.py
@@ -0,0 +1,71 @@
+import numpy
+from random import randint
+class Formiga:
+    def __init__(self):
+        self.node = None
+
+    def proximoNo(self,caminho):
+        self.node = caminho
+
+    def deixarFeromonio(self,Caminho):
+        Caminho.T = Caminho.T + 10 
+
+
+class Caminho:
+    def __init__(self, inicio, fim):
+        self.inicio = inicio
+        self.fim = fim
+        self.T = 10
+        self.N = 1
+ 
+
+    def evaporar(self):
+        self.T = self.T*0.9
+
+
+
+    
+
+class ACO:
+    def __init__(self,tamanho_ninho):
+        self.tamanho_ninho = tamanho_ninho
+        self.trilha = None
+        self.soma_p = 0
+
+    
+    def gerarNinho(self):
+        return [ Formiga for _ in range(self.tamanho_ninho)]
+    
+    def gerarTrilha(self):
+        trilha = []
+        for i in range(-5000, 5001):
+            trilha.append(Caminho(0,i/100))
+        self.trilha = trilha
+
+        
+        
+
+    def fitness(x):
+        return pow(x,2) - 100
+    
+    def gerarDenominador(self):
+        for caminho in self.trilha:
+            a = randint(0,2)
+            b = randint(0,2)
+            T = caminho.T
+            N = caminho.N
+            self.soma_p = self.soma_p + pow(T, a)*pow(N, b)
+    
+    def probalidade(self,caminho):
+        a = randint(0,2)
+        b = randint(0,2)
+        T = caminho.T
+        N = caminho.N
+        return (pow(T, a)*pow(N, b))/(self.soma_p)
+    
+    
+    if __name__ == '__main__':
+
+        aco = ACO(100)
+
+    
